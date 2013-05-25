3fb0d6342c5390a1a63401fdc3f822a0ddad4f08
YvesBill <fabriziobilleci7@gmail.com>
2013-05-25 18:24:07 +0300

    Create Setting.java

---
+++ src/com/YvesB/Nexus/Setting.java
@@ -0,0 +1,14 @@
+package com.YvesB.Nexus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Setting extends Activity {
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.setting);
+  }
+}
