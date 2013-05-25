eec17807575df3c734fe440197c5e61bd14037d1
YvesBill <fabriziobilleci7@gmail.com>
2013-05-25 17:23:37 +0200

    Create Page8.java

---
+++ src/com/YvesB/Nexus/Page8.java
@@ -0,0 +1,13 @@
+package com.YvesB.Nexus;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Page8 extends Activity {
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.page8);
+  }
+}
