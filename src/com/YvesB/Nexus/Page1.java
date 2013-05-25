c57d9c15c1e151821ee1e40d7c6a2f755c40ca2d
YvesBill <fabriziobilleci7@gmail.com>
2013-05-25 18:21:02 +0300

    Create Page1.java

---
+++ src/com/YvesB/Nexus/Page1.java
@@ -0,0 +1,14 @@
+package com.YvesB.Nexus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Page1 extends Activity {
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.page1);
+  }
+}
