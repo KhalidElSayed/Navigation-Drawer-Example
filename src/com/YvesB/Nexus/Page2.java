130c2382102d712495c74d83246489aba963dff9
YvesBill <fabriziobilleci7@gmail.com>
2013-05-26 21:40:07 +0300

    Update Page2.java

--- src/com/YvesB/Nexus/Page2.java
+++ src/com/YvesB/Nexus/Page2.java
@@ -1,6 +1,12 @@
 package com.YvesB.Nexus;
+
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 public class Page2 extends Activity {
@@ -9,5 +15,34 @@ public class Page2 extends Activity {
   public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.page2);
+           ActionBar ab = getActionBar();
+          ab.setHomeButtonEnabled(true);
+          ab.setDisplayHomeAsUpEnabled(true);
   }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+          // TODO Auto-generated method stub
+          MenuInflater mMenuInflater = getMenuInflater();
+          mMenuInflater.inflate(R.menu.main, menu);
+          
+          return true;
+  }
+  
+ 
+      @Override
+      public boolean onOptionsItemSelected(MenuItem item) {
+              switch (item.getItemId()) {
+              case android.R.id.home:
+                      Intent intent = new Intent(this, MainActivity.class);
+                      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                      startActivity(intent);
+                      return true;
+             
+              default:
+                      return super.onOptionsItemSelected(item);
+
+              }
+      }
+
+  
 }
